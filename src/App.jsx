db3af0b8eee1dac0a309f9b38cd0e58048e3ed7f
rajareddykodakandla <rajareddykodakandla@gmail.com>
2025-01-10 13:38:56 -0500

    wrapper components

--- src/App.jsx
+++ src/App.jsx
@@ -2,52 +2,20 @@ import { useState, Fragment, memo } from "react";
 import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      title: "gym",
-      description: "go to gym",
-    },
-    {
-      id: 2,
-      title: "eat",
-      description: "eat food",
-    },
-  ]);
-
-  const addTodo = (e) => {
-    e.preventDefault();
-    setTodos((previousTodos) => {
-      return [
-        ...previousTodos,
-        { id: Math.random(), title: Math.random(), description: Math.random() },
-      ];
-    });
-  };
-
   return (
     <div>
-      <button onClick={addTodo}>Add todo</button>
-      {todos.map((todo) => {
-        return (
-          <Todo
-            key={todo.id}
-            title={todo.title}
-            description={todo.description}
-          ></Todo>
-        );
-      })}
+      <CardWrapper>
+        <div>
+          <h1>Hi there</h1>
+        </div>
+      </CardWrapper>
     </div>
   );
 }
 
-function Todo({ title, description }) {
-  return (
-    <div>
-      <h1>{title}</h1>
-      <h5>{description}</h5>
-    </div>
-  );
+function CardWrapper({ children }) {
+  console.log("children", children);
+  return <div style={{ border: "2px solid black" }}>{children}</div>;
 }
 
 export default App;
