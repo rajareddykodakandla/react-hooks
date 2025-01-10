0b26745af8e4d6e260d2e3ce922eb51f2d22cbcb
rajareddykodakandla <rajareddykodakandla@gmail.com>
2025-01-10 13:20:50 -0500

    keys in react

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,52 @@ import { useState, Fragment, memo } from "react";
 import "./App.css";
 
 function App() {
-  const [name, setName] = useState("raja");
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "gym",
+      description: "go to gym",
+    },
+    {
+      id: 2,
+      title: "eat",
+      description: "eat food",
+    },
+  ]);
 
-  const changeName = () => {
-    setName(() => {
-      return parseInt(Math.random() * 10);
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos((previousTodos) => {
+      return [
+        ...previousTodos,
+        { id: Math.random(), title: Math.random(), description: Math.random() },
+      ];
     });
   };
+
   return (
     <div>
-      <button onClick={changeName}>Click me to change the name</button>
-      <Header name={name}></Header>
-      <Header name="Reddy" />
-      <Header name="Reddy" />
-      <Header name="Reddy" />
-      <Header name="Reddy" />
-      <Header name="Reddy" />
-      <Header name="Reddy" />
+      <button onClick={addTodo}>Add todo</button>
+      {todos.map((todo) => {
+        return (
+          <Todo
+            key={todo.id}
+            title={todo.title}
+            description={todo.description}
+          ></Todo>
+        );
+      })}
     </div>
   );
 }
 
-const Header = memo(function Header({ name }) {
+function Todo({ title, description }) {
   return (
     <div>
-      <h1>My name is {name}</h1>
+      <h1>{title}</h1>
+      <h5>{description}</h5>
     </div>
   );
-});
+}
 
 export default App;
